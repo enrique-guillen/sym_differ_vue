c6fe797e11148427ac9f1e06d4cbe0047731503f
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-10 13:09:30 +0900

    New SymDifferNamedToken component

---
+++ src/components/SymDifferNamedToken.vue
@@ -0,0 +1,19 @@
+<script setup>
+  defineProps({
+    token_name: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  &lt;<span class="reference-token-name">{{ token_name }}</span>&gt;
+</template>
+
+<style>
+  .reference-token-name {
+    color: #3333ff;
+    font-weight: 600;
+  }
+</style>
