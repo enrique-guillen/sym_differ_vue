817e45f2be8cb947a1290ed92b679b025547ea09
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-11 16:54:04 +0900

    Rewrite user-requests-derivative-expression with ES6 exports

--- src/user-requests-derivative-expression.js
+++ src/user-requests-derivative-expression.js
@@ -1,29 +1,44 @@
-module.exports = {
-  userRequestsDerivativeExpression: (deriver, expressionText, variable, successCallback, failureCallback) => {
-    deriver.derive(
-      expressionText, variable, successCallback, failureCallback
+function requestDerivativeExpression(deriver, expressionText, variable, successCallback, failureCallback) {
+  userRequestsDerivativeExpression(
+    { derive: deriver },
+    expressionText,
+    variable,
+    makeSuccessfulResponseCallback({ handle: successCallback }),
+    makeFailureResponseCallback({ handle: failureCallback })
+  )
+}
+
+function userRequestsDerivativeExpression(deriver, expressionText, variable, successCallback, failureCallback) {
+  deriver.derive(expressionText, variable, successCallback, failureCallback)
+}
+
+function makeSuccessfulResponseCallback(handler) {
+  return (response) => {
+    handler.handle(
+      {
+        derivativeExpressionText: response.derivativeExpressionText,
+        showGetExpressionDerivativeFailed: false,
+        showGetExpressionDerivativeSucceeded: true
+      }
     )
-  },
-  makeSuccessfulResponseCallback: (handler) => {
-    return (response) => {
-      handler.handle(
-        {
-          derivativeExpressionText: response.derivativeExpressionText,
-          showGetExpressionDerivativeFailed: false,
-          showGetExpressionDerivativeSucceeded: true
-        }
-      )
-    }
-  },
-  makeFailureResponseCallback: (handler) => {
-    return (response) => {
-      handler.handle(
-        {
-          message: response.message,
-          showGetExpressionDerivativeFailed: true,
-          showGetExpressionDerivativeSucceeded: false
-        }
-      )
-    }
   }
+}
+
+function makeFailureResponseCallback(handler) {
+  return (response) => {
+    handler.handle(
+      {
+        message: response.message,
+        showGetExpressionDerivativeFailed: true,
+        showGetExpressionDerivativeSucceeded: false
+      }
+    )
+  }
+}
+
+export {
+  requestDerivativeExpression,
+  userRequestsDerivativeExpression,
+  makeSuccessfulResponseCallback,
+  makeFailureResponseCallback
 }
