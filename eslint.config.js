e3830b7b21b3535c897d82aa47707b7525d3df50
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-12 11:34:50 +0900

    Curly spacing eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,27 @@
 import globals from 'globals';
 import pluginVue from 'eslint-plugin-vue';
+import stylistic from '@stylistic/eslint-plugin';
 
 import path from 'path';
-import {fileURLToPath} from 'url';
-import {FlatCompat} from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 
 // Mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-  {languageOptions: {globals: globals.browser}},
+  { languageOptions: { globals: globals.browser } },
   ...compat.extends('xo'),
   ...pluginVue.configs['flat/essential'],
-  {rules: {indent: ['error', 2]}},
+  { plugins: { '@stylistic': stylistic } },
+  {
+    rules: {
+      indent: ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+    },
+  },
 ];
