9d32b1d97353908675604829101c890a434f27bd
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-12 11:45:16 +0900

    Remove unusued parms method in axios-adapter

--- src/axios-adapter.js
+++ src/axios-adapter.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 function derive(expressionText, variable, successCallback, failureCallback) {
-  const params = { params: { expression: expressionText, variable } };
   axios.get(
     'http://127.0.0.1:3000/derivative_expression',
     { params: { expression: expressionText, variable } },
