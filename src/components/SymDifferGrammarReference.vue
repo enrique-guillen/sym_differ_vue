accebfa49f1d95bb70bcc6a876e6037b9b9228e5
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-16 15:48:11 +0900

    Fuller Grammar reference

--- src/components/SymDifferGrammarReference.vue
+++ src/components/SymDifferGrammarReference.vue
@@ -12,16 +12,23 @@ import NamedToken from './SymDifferNamedToken.vue';
 
     <p>
       The following is a reference of our expression language in Backus-Naur form. A list of words separated by |
-      characters represent alternatives, [0-9] represents any number from 0 to 9, [a-zA-Z] represents any alphabetical
-      character, and a + character indicates that the text is valid (and categorized in the corresponding item in the
-      list) if it consists of at least 1 or more instances of the characters specified before the symbol appears.
+      characters represent alternatives.
     </p>
     <ul>
       <li>
         • <NamedToken tokenName="expression" /> ::=
             <NamedToken tokenName="constant" /> |
             <NamedToken tokenName="variable" /> |
-            <NamedToken tokenName="sum" />
+            <NamedToken tokenName="sum" /> |
+            <NamedToken tokenName="subtraction" />
+      </li>
+      <li>
+        • <NamedToken tokenName="sum" /> ::=
+            <NamedToken tokenName="expression" /> + <NamedToken tokenName="expression" />
+      </li>
+      <li>
+        • <NamedToken tokenName="subtraction" /> ::=
+            <NamedToken tokenName="expression" /> - <NamedToken tokenName="expression" />
       </li>
       <li>
         • <NamedToken tokenName="constant" /> ::= [0-9]+
@@ -29,32 +36,19 @@ import NamedToken from './SymDifferNamedToken.vue';
       <li>
         • <NamedToken tokenName="variable" /> ::= [a-zA-Z]+
       </li>
-      <li>
-        • <NamedToken tokenName="sum" /> ::=
-            <NamedToken tokenName="expression" /> + <NamedToken tokenName="expression" />
-      </li>
     </ul>
+
+    <p>
+      [0-9]+ matches a number with 1 or more digits. [a-zA-Z]+ Matches a string with 1 or more characters.
+    </p>
+
+    <p>
+      Click <RouterLink to="/">here</RouterLink> to go back to the main page.
+    </p>
   </div>
 </template>
 
 <style>
-  .sym-differ-reference {
-    font-family:
-      'Segoe UI',
-      Cambria,
-      Inter,
-      math,
-      -apple-system,
-      BlinkMacSystemFont,
-      Roboto,
-      Oxygen,
-      Ubuntu,
-      Cantarell,
-      'Fira Sans',
-      'Droid Sans',
-      'Helvetica Neue',
-      sans-serif;
-  }
   .sym-differ-reference h2 {
     border-bottom: 2px solid var(--color-border);
     margin-bottom: 1rem;
@@ -62,6 +56,7 @@ import NamedToken from './SymDifferNamedToken.vue';
   .sym-differ-reference ul {
     list-style-type: none;
     font-size: 1.1rem;
+    margin-bottom: 1rem;
   }
   .sym-differ-reference p {
     margin-bottom: 1rem;
