f0c1ba45376e7a8fcf318ffd1394c488d5263f7c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-16 15:49:32 +0900

    New SymDifferHome vue component

---
+++ src/components/SymDifferHome.vue
@@ -0,0 +1,37 @@
+<script setup>
+import SymDifferSeeGrammarReferenceHint from './SymDifferSeeGrammarReferenceHint.vue';
+import GetDerivativeExpressionForm from './GetDerivativeExpressionForm.vue';
+</script>
+
+<template>
+  <div class="sym-differ-home">
+    <GetDerivativeExpressionForm />
+    <SymDifferSeeGrammarReferenceHint />
+  </div>
+</template>
+
+<style>
+  .sym-differ-home {
+    max-width: 1280px;
+    margin: auto auto;
+    padding: 2rem;
+    font-weight: normal;
+  }
+
+  @media (min-width: 1024px) {
+    .sym-differ-home {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      padding: 0 2rem;
+    }
+  }
+
+  .sym-differ-reference-hint {
+    margin: auto 1.5rem;
+  }
+
+  form {
+    margin: 1.5rem;
+    font-size: 1.1rem;
+  }
+</style>
