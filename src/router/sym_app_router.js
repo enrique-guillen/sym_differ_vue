b94f1b7fe42e5dd093445b065afcd3ea93e039e4
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-16 15:48:44 +0900

    Define alternative router

---
+++ src/router/sym_app_router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../components/SymDifferHome.vue'),
+    },
+    {
+      path: '/grammarreference',
+      name: 'grammarreference',
+      component: () => import('../components/SymDifferGrammarReference.vue'),
+    },
+  ],
+});
+export default router;
