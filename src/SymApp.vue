6212f5b4740cdc26c61da11985e713a615aed7a6
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-16 15:52:03 +0900

    Define default transition for SymApp

--- src/SymApp.vue
+++ src/SymApp.vue
@@ -4,18 +4,22 @@ import { RouterView } from 'vue-router';
 
 <template>
   <RouterView v-slot="{ Component }">
-    <Transition>
+    <Transition name="sym-differ-current-component">
       <component :is="Component" />
     </Transition>
   </RouterView>
 </template>
 
 <style>
-  .sym-differ-reference {
-    margin: 1.5rem;
-  }
-  form {
-    margin: 1.5rem;
-    font-size: 1.1rem;
-  }
+.sym-differ-current-component-enter-active {
+  transition: opacity 1.5s ease;
+}
+
+.sym-differ-current-component-enter-from {
+  opacity: 0;
+}
+
+.sym-differ-current-component-leave-from {
+  opacity: 0;
+}
 </style>
