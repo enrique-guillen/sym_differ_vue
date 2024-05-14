35eca32484ce489bcb16d1fe01abc9d8b14852fd
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-14 20:05:10 +0900

    New axios-adapter.js #generateImage

--- src/axios-adapter.js
+++ src/axios-adapter.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 function derive(expressionText, variable, successCallback, failureCallback) {
-  axios.get(
-    'http://127.0.0.1:3000/derivative_expression',
-    { params: { expression: expressionText, variable } },
-  )
-    .then(response => {
-      successCallback({ derivativeExpressionText: response.data.derivative_expression });
-    })
-    .catch(error => {
-      failureCallback({ message: error.response.data.message });
-    });
+  const derivative_expression_url = 'http://127.0.0.1:3000/derivative_expression';
+
+  axios
+    .get(derivative_expression_url, { params: { expression: expressionText, variable } },)
+    .then(response => { successCallback({ derivativeExpressionText: response.data.derivative_expression }); })
+    .catch(error => { failureCallback({ message: error.response.data.message }); });
 }
 
-export { derive };
+function generateImage(expressionText, variable, successCallback) {
+  const derivative_expression_visualization_url = 'http://127.0.0.1:3000/derivative_expression_visualization';
+
+  axios
+    .get(derivative_expression_visualization_url, { params: { expression: expressionText, variable } },)
+    .then(response => { successCallback(response.data); })
+}
+
+export { derive, generateImage };
