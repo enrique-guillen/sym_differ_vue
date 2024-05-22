ba38a9187477174ec1e29a349f68183d422bfca5
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-23 06:18:02 +0900

    Provide differential_equation_illustrator plugin to SymApp from main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia';
 import deriver from './plugins/deriver.js';
 import imager from './plugins/imager.js';
 import firstOrderDifferentialEquationApproximator from './plugins/first_order_differential_equation_approximator.js';
+import differentialEquationIllustrator from './plugins/differential_equation_illustrator.js';
 
 import App from './SymApp.vue';
 import router from './router/sym_app_router.js';
@@ -18,5 +19,6 @@ app.use(router);
 app.use(deriver);
 app.use(imager);
 app.use(firstOrderDifferentialEquationApproximator);
+app.use(differentialEquationIllustrator);
 
 app.mount('#app');
